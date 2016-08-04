4c4265c741a62b2d4bef1929fd57a1c6cbb81423
felix.lin <fly_81211@hotmail.com>
2016-08-04 23:50:28 +0800

    Added enum type.

--- IUpgrade/Classes/IUpgrade.h
+++ IUpgrade/Classes/IUpgrade.h
@@ -7,9 +7,17 @@
 //
 
 #import <Foundation/Foundation.h>
+typedef NS_ENUM(NSUInteger, IUpgradeAlertType)
+{
+    IUpgradeDefault,
+    IUpgradeForec,
+    IUpgradeSkip
+};
 
 @interface IUpgrade : NSObject
 @property (nonatomic) NSString *plistUrlString;
+@property (nonatomic, assign) IUpgradeAlertType type;
+
 + (IUpgrade *)sharedInstance;
 - (void)checkVersion;
 @end
--- IUpgrade/Classes/IUpgrade.m
+++ IUpgrade/Classes/IUpgrade.m
@@ -8,8 +8,9 @@
 
 #import "IUpgrade.h"
 
+NSString * const IUpgradeStoredVersionData = @"Upgrade Stored Version Data";
+
 @interface IUpgrade ()
-@property (nonatomic, copy) NSString *currentInstalledVersion;
 @property (nonatomic, strong) NSDictionary <NSString *, id> *appData;
 @end
 
@@ -35,9 +36,10 @@
     self = [super init];
     
     if (self) {
+        _type = IUpgradeDefault;
 //        _alertType = HarpyAlertTypeOption;
 //        _lastVersionCheckPerformedOnDate = [[NSUserDefaults standardUserDefaults] objectForKey:HarpyDefaultStoredVersionCheckDate];
-        _currentInstalledVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+//        _currentInstalledVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
     }
     
     return self;
@@ -74,10 +76,39 @@
     NSPropertyListFormat format;
     _appData = [NSPropertyListSerialization propertyListWithData:data  options:NSPropertyListImmutable format:&format error:&error];
     
-
-    NSLog( @"plist is %@", _appData );
     if(!_appData){
-        NSLog(@"Error: %@",error);
+        NSLog(@"Plist fetch error: %@",error);
+    }
+
+    if (![self isCompatibleWithBundleID:_appData]) {
+        NSLog(@"Not compatible with bundle ID");
+    }
+    if (![self isNeedToUpdateVersion:_appData]) {
+        NSLog(@"Not need to upgrade version");
+    }
+    [self showAlertWithNewVersion:_appData];
+}
+
+- (void)showAlertWithNewVersion:(NSDictionary<NSString *, id> *)appData {
+    
+}
+
+
+- (BOOL)isCompatibleWithBundleID:(NSDictionary<NSString *, id> *)appData {
+    return (appData[@"items"][0][@"metadata"][@"bundle-identifier"] == [NSBundle mainBundle].bundleIdentifier);
+}
+
+- (BOOL)isNeedToUpdateVersion:(NSDictionary<NSString *, id> *)appData {
+    NSString* newVersion = appData[@"items"][0][@"metadata"][@"bundle-version"];
+    NSString* currentVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"];
+   
+    if (
+        ([newVersion compare:currentVersion options:NSNumericSearch] == NSOrderedDescending) ||
+        ([newVersion compare:currentVersion options:NSNumericSearch] == NSOrderedSame)
+        ) {
+        return true;
+    } else {
+        return false;
     }
 }
 @end
